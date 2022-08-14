107743552e10a780363d8c742e3017333b56a23c
Tushar Nath <tusharnath10@gmail.com>
2022-08-14 23:56:15 +0530

    Update ApiService.js

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://serene-reef-80586.herokuapp.com';
 
 /* GET request URLs*/
 const COVID_TRACKER_API_URL = `${BASE_URL}/covid-api/in`;
